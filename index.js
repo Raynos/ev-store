a20cc3c9a1a0d32995b50e9092600f459f8946a2
Raynos <raynos2@gmail.com>
2014-12-21 04:40:45 -0800

    initial yolo

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var hashKey = '__EV_STORE_KEY@4';
+
+module.exports = EvStore;
+
+function EvStore(elem) {
+    var hash = elem[hashKey];
+
+    if (!hash) {
+        hash = elem[hashKey] = {};
+    }
+
+    return hash;
+}
