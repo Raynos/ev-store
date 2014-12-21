4cca3c9af5366cafa3405fb5f23a8c3521b712ba
Raynos <raynos2@gmail.com>
2014-12-21 04:49:48 -0800

    add a version check

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 'use strict';
 
-var hashKey = '__EV_STORE_KEY@6';
+var flagKey = '__EV_STORE_ENFORCE_SINGLETON';
+var MY_VERSION = '6';
+var globalScope = getGlobalScope();
+
+if (globalScope[flagKey]) {
+    var version = globalScope[flagKey];
+    throw new Error('Can only have one copy of ev-store.\n' +
+        'You already have version ' + version + ' installed.\n' +
+        'This means you cannot install version ' + MY_VERSION);
+}
+
+globalScope[flagKey] = MY_VERSION;
+
+var hashKey = '__EV_STORE_KEY@' + MY_VERSION;
 
 module.exports = EvStore;
 
@@ -12,4 +25,9 @@ function EvStore(elem) {
     }
 
     return hash;
+}
+
+function getGlobalScope() {
+    return typeof global !== 'undefined' ? global :
+        typeof window !== 'undefined' ? window : {};
 }
